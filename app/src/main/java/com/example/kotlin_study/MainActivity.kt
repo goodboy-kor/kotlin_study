3ed102063444b649fd3b5e92f96bdb6ec2f46e07
WESEOKHWAN <llaayy.kr@gmail.com>
2021-02-16 02:54:25 +0900

    test

--- app/src/main/java/com/example/kotlin_study/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_study/MainActivity.kt
@@ -2,10 +2,13 @@ package com.example.kotlin_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Log.d("로그", "테스트 입니다.");
     }
 }
