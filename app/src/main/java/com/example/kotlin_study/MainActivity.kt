e7288cd10a818d1661e703a4c78acb29b1f3d787
WESEOKHWAN <llaayy.kr@gmail.com>
2021-02-18 02:19:10 +0900

    start0217

--- app/src/main/java/com/example/kotlin_study/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_study/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.kotlin_study
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,6 +12,9 @@ class MainActivity : AppCompatActivity() {
 
         Log.d("로그", "두번째 branch 입니다.");
         Log.d("로그", "두번째 branch merge 테스트 입니다.");
-        //merge 종료
+
+        var tab3 = tablayout.newTab()
+        tab3.setText("tab 3")
+        tablayout.addTab(tab3)
     }
 }
