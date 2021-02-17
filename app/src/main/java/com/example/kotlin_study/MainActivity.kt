7ebe0bf31e58d719ce2511b43c535608752e12b7
WESEOKHWAN <llaayy.kr@gmail.com>
2021-02-18 02:19:55 +0900

    blank activity for study

--- app/src/main/java/com/example/kotlin_study/MainActivity.kt
+++ app/src/main/java/com/example/kotlin_study/MainActivity.kt
@@ -9,12 +9,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.d("로그", "두번째 branch 입니다.");
-        Log.d("로그", "두번째 branch merge 테스트 입니다.");
-
-        var tab3 = tablayout.newTab()
-        tab3.setText("tab 3")
-        tablayout.addTab(tab3)
     }
 }
